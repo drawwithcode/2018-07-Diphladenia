c344a5d27fc3d6726b1464211adbf81887e8d852
Diphladenia <43388850+Diphladenia@users.noreply.github.com>
2018-11-06 22:38:16 +0100

    final code

--- sketch.js
+++ sketch.js
@@ -1,13 +1,53 @@
-function preload(){
-  // put preload code here
-}
+var mic;
+var analyzer;
+var capture;
+
 
 function setup() {
-  createCanvas(500,500);
-  background(200);
-  // put setup code here
+  createCanvas(800, 800);
+  capture = createCapture(VIDEO);
+	capture.size(500, 375);
+	capture.hide();
+  background(255);
+
+  //Create an Audio input
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
 function draw() {
-  // put drawing code here
+  // Get the overall volume (between 0 and 1.0)
+  var volume = mic.getLevel();
+
+  // If the volume > 0.1,  a circle is drawn at a random location.
+  // The louder the volume, the larger the circle.
+  var threshold = 0.1;
+  if (volume > threshold) {
+    stroke(255);
+    strokeWeight(random(1,4));
+    fill(0);
+    ellipse(random(width), random(height), volume*40, volume*40);
+  }
+
+  //webcam changes filter
+	var myImage = capture.loadPixels();
+  imageMode(CENTER);
+	image(myImage, width/2, height/2, 600, 600);
+    if (volume>0 && volume<0.15) {
+  	filter(THRESHOLD);
+  	fill(random(255),random(255),random(255));
+  	ellipse(random(width),random(height),volume*40, volume*40);
+  }  else if (volume>0.15 && volume<.2) {
+  	filter(POSTERIZE,4);
+  	fill(random(255),random(255),random(255));
+  	ellipse(random(width),random(height),volume*40, volume*40);
+  }  else if (volume>0.2 && volume<0.4) {
+  	filter(INVERT);
+    fill(random(255),random(255),random(255));
+    ellipse(random(width),random(height),volume*40, volume*40);
+  } else if (volume>0.4 && volume<0.6) {
+    	filter(GRAY);
+      fill(random(255),random(255),random(255));
+      ellipse(random(width),random(height),volume*40, volume*40);
+    	}
 }
